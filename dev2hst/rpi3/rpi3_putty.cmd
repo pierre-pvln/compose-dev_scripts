90dfb4b5ad7d07616d8e153b693a87e4aef9b7bf
Pierre <pierre@pvln.nl>
2018-06-11 09:24:15 +0200

    batch scripts updated

--- dev2hst/rpi3/rpi3_putty.cmd
+++ dev2hst/rpi3/rpi3_putty.cmd
@@ -2,7 +2,7 @@ SET connectport=22
 SET vps-hostname=192.168.2.11
 SET _putty="C:\Program Files\PuTTY\putty.exe"
 
-%_putty% -ssh -P %connectport% -pw PVeeRPI3 pi@%vps-hostname%
+%_putty% -ssh -P %connectport% -pw PVee#RPI3 pi@%vps-hostname%
 
 #
 # if you forgot the password, but RPI3 boots to command line or you can open terminal window 
